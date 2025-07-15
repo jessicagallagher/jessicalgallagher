d416b5a6e642e900b4b77e273592c9c3f3adeb81
jessicagallagher <jessicalyngallagher@me.com>
2025-07-15 14:03:57 -0400

    add cursor effect and fix hydration warning in background

--- app/components/index.js
+++ app/components/index.js
@@ -1,7 +1,9 @@
+import FluidCursor from './FluidCursor';
 import NoRightClick from './NoRightClick';
 import TwinklingStars from './TwinklingStars';
 
 export {
+  FluidCursor,
   NoRightClick,
   TwinklingStars,
 }
--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang='en'>
       <body>
         <NoRightClick>
-          <TwinklingStars>{children}</TwinklingStars>
+          {children}
         </NoRightClick>
       </body>
     </html>
