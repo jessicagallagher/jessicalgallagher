21b3e6e2c3fcd2fe92a10191878a0fc489c31086
jessicagallagher <jessicalyngallagher@me.com>
2025-08-14 13:37:06 -0400

    change list items on about page to have emoji instead of bullet points

--- app/components/PortfolioPage.jsx
+++ app/components/PortfolioPage.jsx
@@ -1,11 +1,12 @@
 'use client'
-import { NavBar } from '.'
+import { NavBar, Typewriter } from '.'
 
 export default function PortfolioPage() {
   return (
     <>
       <div className='h-screen max-w-full'>
         <NavBar />
+        <Typewriter text='Portfolio' delay={100} />
       </div>
     </>
   );
