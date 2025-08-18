92f39dead3befddc9fef10c09183b27241c84918
jessicagallagher <jessicalyngallagher@me.com>
2025-08-18 14:48:16 -0400

    organize project data and finish portfolip page

--- app/components/PortfolioPage.jsx
+++ app/components/PortfolioPage.jsx
@@ -1,78 +1,90 @@
 'use client'
-import { useState } from 'react';
 import { NavBar, Typewriter } from '.'
+import { projects, funProjects } from '.././utils/portfolio';
 
 export default function PortfolioPage() {
-  const [popup, showPopup] = useState(false);
-
-  const projects = [
-    {
-      name: 'Tech Meowt',
-      imgUrl: '',
-      techUsed: 'Next.js, Tailwind CSS, Resend, + Setmore',
-      link: 'https://www.techmeowt.com',
-      comments: 'This website was built and maintained for Tech Meowt.',
-      showPopup: false,
-    },
-    {
-      name: 'Southpaw Martial Arts and Fitness Club',
-      imgUrl: '',
-      techUsed: 'Next.js, Tailwind CSS, + Resend',
-      link: 'https://www.southpawmartialarts.com',
-      comments:
-        'This website was built and maintained for Southpaw Martial Arts and Fitness Club.',
-      showPopup: false,
-    },
-    {
-      name: 'Petsitting application',
-      imgUrl: '',
-      techUsed: 'Next.js, Tailwind CSS, Supabase, Prismic CMS + Mapbox',
-      link: '#',
-      comments: `This application was built and maintained for a private client and can't be accessed by the public.`,
-      showPopup: true,
-    },
-    {
-      name: 'Realtor.com clone',
-      imgUrl: '',
-      techUsed: 'React, Tailwind CSS, Firebase',
-      link: 'https://realtor-clone-tan.vercel.app/',
-      comments: 'This is a clone of the Realtor.com website.',
-      showPopup: false,
-    },
-    {
-      name: 'Database application',
-      imgUrl: '',
-      techUsed: 'React, Bootstrap, MongoDB, Express, Prisma ORM, + Nodemailer',
-      link: '',
-      comments: `This application was built and maintained for a private client and can't be accessed by the public.`,
-      showPopup: true,
-    },
-  ];
-
-  const funProjects = [
-    {
-      name: 'Google Chrome dinosaur game clone',
-      imgUrl: '',
-      techUsed: 'Vanilla JavaScript',
-      link: 'https://chrome-dino-game-ten.vercel.app/',
-      comments:
-        'This game is accessible on both web and mobile browsers. Use your spacebar to jump on the web version, and tap to jump on the mobile version.',
-    },
-    {
-      name: 'Tetris clone',
-      imgUrl: '',
-      techUsed: 'React',
-      link: 'https://jess-tetris.vercel.app/',
-      comments:
-        'This game is accessible on web browsers only. Hold down the arrow key to quickly drop the tetromino, press the left and right arrow keys to position the tetromino, and press the up arrow key to rotate the tetromino.',
-    },
-  ];
-
   return (
     <>
       <div className='h-screen max-w-full'>
         <NavBar />
         <Typewriter text='Portfolio' delay={100} />
+        <div className='h-full max-w-full mx-auto px-2 lg:px-10'>
+          <ul role='list' className='flex flex-col space-y-12 justify-center'>
+            {projects.map((project) => (
+              <li key={project.name}>
+                <div className='space-y-4'>
+                  <div>
+                    <img
+                      className='rounded-lg object-cover w-1/2 h-full border border-white mx-auto'
+                      src={project.imgUrl}
+                      alt={`Screenshot of project: ${project.name}`}
+                    />
+                  </div>
+                  <div className='space-y-2'>
+                    <div className='space-y-1 font-semibold leading-6 text-center'>
+                      <a
+                        href={project.link}
+                        className={`${
+                          project.link === '#'
+                            ? 'cursor-not-allowed'
+                            : 'cursor-pointer'
+                        }`}
+                        target='_blank'
+                        rel='noopener'
+                      >
+                        <h3 className='text-lg md:text-xl hover:text-4xl'>
+                          {project.name}
+                        </h3>
+                      </a>
+                      <p className='font-semibold'>
+                        Built with {project.techUsed}
+                      </p>
+                      <p className='font-medium'>{project.comments}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <Typewriter text='Just for Fun' delay={100} />
+          <ul role='list' className='flex flex-col space-y-12 justify-center'>
+            {funProjects.map((project) => (
+              <li key={project.name}>
+                <div className='space-y-4'>
+                  <div>
+                    <img
+                      className='rounded-lg object-cover w-1/2 h-full border border-white mx-auto'
+                      src={project.imgUrl}
+                      alt={`Screenshot of project: ${project.name}`}
+                    />
+                  </div>
+                  <div className='space-y-2'>
+                    <div className='space-y-1 font-semibold leading-6 text-center mx-24 lg:mx-48'>
+                      <a
+                        href={project.link}
+                        className={`${
+                          project.link === '#'
+                            ? 'cursor-not-allowed'
+                            : 'cursor-pointer'
+                        }`}
+                        target='_blank'
+                        rel='noopener'
+                      >
+                        <h3 className='text-lg md:text-xl hover:text-4xl'>
+                          {project.name}
+                        </h3>
+                      </a>
+                      <p className='font-semi-bold'>
+                        Built with {project.techUsed}
+                      </p>
+                      <p className='font-medium'>{project.comments}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
